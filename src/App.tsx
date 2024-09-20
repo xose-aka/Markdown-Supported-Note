1a5f86803e1a813dfdaa24764988f4159d954cdd
xose-aka <xusniddin-qodirov@mail.ru>
2024-09-20 22:39:22 +0200

    New Note Forum bind state and form controls, declare form types

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,26 @@ import {Navigate, Route, Routes} from "react-router-dom";
 import {Container} from "react-bootstrap";
 import NewNote from "./components/NewNote.tsx";
 
+export type Note = {
+    id: string
+} & NoteData
+
+export type NoteData = {
+    title: string
+    markdown: string
+    tags: Tag[]
+}
+
+export type Tag = {
+    id: string
+    label: string
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
+
+
+
 
   return (
       <Container className={'my-4'}>
--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,27 +1,57 @@
-import {Col, Form, Row, Stack} from "react-bootstrap";
+import {Button, Col, Form, Row, Stack} from "react-bootstrap";
 import CreatableSelect from "react-select/creatable";
+import {Link} from "react-router-dom";
+import {FormEvent, useRef, useState} from "react";
+import {NoteData, Tag} from "../App.tsx";
 
-export function NoteForm() {
-    return <Form>
+type NoteFormProps = {
+    onSubmit: ( data: NoteData ) => void
+}
+
+export function NoteForm({ onSubmit }: NoteFormProps) {
+
+    const titleRef = useRef<HTMLInputElement>(null);
+    const markdownRef = useRef<HTMLTextAreaElement>(null);
+    const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+
+        onSubmit({
+            title: titleRef.current!.value,
+            markdown: markdownRef.current!.value,
+            tags: []
+        })
+    }
+
+    return <Form onSubmit={ handleSubmit }>
         <Stack gap={3}>
             <Row>
                 <Col>
                     <Form.Group controlId="title">
                         <Form.Label>Title</Form.Label>
-                        <Form.Control required />
+                        <Form.Control ref={titleRef} required />
                     </Form.Group>
                 </Col>
                 <Col>
                     <Form.Group controlId="tags">
                         <Form.Label>Tags</Form.Label>
-                        <CreatableSelect isMulti />
+                        <CreatableSelect value={selectedTags.map( tag => {
+                            return { label: tag.label, value: tag.id }
+                        } )} isMulti />
                     </Form.Group>
                 </Col>
             </Row>
             <Form.Group controlId="markdown">
                 <Form.Label>Body</Form.Label>
-                <Form.Control required as="textarea" rows={15}/>
+                <Form.Control ref={markdownRef} required as="textarea" rows={15}/>
             </Form.Group>
+            <Stack direction="horizontal" gap={2} className="justify-content-end">
+                <Button type="submit" variant="outline-primary">Save</Button>
+                <Link to={'..'}>
+                    <Button type="button" variant="outline-secondary">Cancel</Button>
+                </Link>
+            </Stack>
         </Stack>
     </Form>
 }
