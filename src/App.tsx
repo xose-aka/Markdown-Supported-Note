17ecb9704330e30b2df5d016001fc301523d86ed
xose-aka <xusniddin-qodirov@mail.ru>
2024-09-21 23:40:40 +0200

    Create new note, useLocalStorage function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
+import {useMemo, useState} from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import {Navigate, Route, Routes} from "react-router-dom";
 import {Container} from "react-bootstrap";
 import NewNote from "./components/NewNote.tsx";
+import {useLocalStorage} from "./useLocalStorage.ts";
+import {v4 as uuidV4} from "uuid";
 
 export type Note = {
     id: string
 } & NoteData
+
+export type RawNote = {
+    id:string
+} & RawNoteData
+
+export type RawNoteData = {
+    title: string
+    markdown: string
+    tagIds: string[]
+}
 
 export type NoteData = {
     title: string
@@ -20,25 +32,35 @@ export type Tag = {
 }
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+    const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
 
+    const notesWithTags = useMemo(() => {
+        return notes.map(note => {
+            return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id))}
+        })
+    }, [notes, tags])
+    
+    function onCreateNote({tags, ...data}: NoteData) {
+        setNotes(prevNotes => {
+            return [...prevNotes, { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) }]
+        })
+    }
 
+    return (
+          <Container className={'my-4'}>
+              <Routes>
+                  <Route path="/" element={<h1>Hi</h1>}></Route>
+                  <Route path="/new" element={<NewNote />}></Route>
+                  <Route path="/:id">
+                      <Route index element={<h1>Show</h1>}></Route>
+                      <Route path={'edit'} element={<h1>Edit</h1>}></Route>
+                  </Route>
+                  <Route path={'*'} element={<Navigate to={'/'}/>}></Route>
+              </Routes>
+          </Container>
 
-
-  return (
-      <Container className={'my-4'}>
-          <Routes>
-              <Route path="/" element={<h1>Hi</h1>}></Route>
-              <Route path="/new" element={<NewNote />}></Route>
-              <Route path="/:id">
-                  <Route index element={<h1>Show</h1>}></Route>
-                  <Route path={'edit'} element={<h1>Edit</h1>}></Route>
-              </Route>
-              <Route path={'*'} element={<Navigate to={'/'}/>}></Route>
-          </Routes>
-      </Container>
-
-  )
+    )
 }
 
 export default App
